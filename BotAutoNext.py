9b11b19a78b612521ae8462fd425919cfa8e23d6
Ruiz05 <RuizTML11@yahoo.com>
2020-07-12 11:10:08 +0800

    update

--- BotAutoNext.py
+++ BotAutoNext.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 Username = 'RuizTML11@yahoo.com'
 Passwd = 'Kalingga69'
 komen = 'Hadir'
-driver = webdriver.Chrome('/home/arif/Desktop/chromedriver_linux64/chromedriver')  # Optional argument, if not specified will search path.
-driver.get('https://umkt.ucm.ac.id/courses/operating-system/pendahuluan/')
+driver = webdriver.Chrome('/home/arif/Desktop/chromedriver_linux64/chromedriver')
+driver.get('https://umkt.ucm.ac.id/courses/networking-infrastruture-management/homepage/')
 cih = driver.find_element_by_class_name('top-bar-menu-login-link')
 cih.click()
 userinput = driver.find_element_by_name('email')
@@ -14,7 +14,8 @@ passinput.send_keys(Passwd)
 tombol = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/div/div/div[3]/div/form/button')
 tombol.click()
 time.sleep(5)
-driver.get('https://umkt.ucm.ac.id/courses/operating-system/g_11/')
+driver.get('https://umkt.ucm.ac.id/courses/networking-infrastruture-management/i_1/')
 while True :
-    time.sleep(5)
+    time.sleep(10)
     nexto = driver.find_element_by_xpath('//*[@id="page-traversal"]/a[2]').click()
+//update
